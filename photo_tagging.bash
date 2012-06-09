8ae0a4d5e859dff6706c5c1b7929973d6ecbcebe
Bartosz Szczesny <bszcz@bszcz.eu>
2012-06-09 14:07:22 +0100

    first two Bash scripts

---
+++ photo_tagging.bash
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# 2011 (CC0) Bartosz Szczesny <bszcz@bszcz.eu>
+
+com="qiv -m"
+
+for i in $(ls)
+do
+  hasz=${i%_*}"_"
+  old_tags=${i#*_}
+  echo "OLD : $hasz$old_tags"
+
+  $com $i &
+  pid=$!
+
+  echo -n "TAG : "
+  read new_tags
+
+  if [ ${new_tags:0:1} == "_" ]
+  then
+    new_tags=$new_tags$old_tags
+  fi
+
+  new_tags=$(echo $new_tags  | awk "{print toupper(\$0)}" | grep -o . | sort | uniq | tr -d "\n _" )
+
+  echo "NEW : $hasz$new_tags"
+
+  mv $i $hasz$new_tags
+  kill -9 $pid 2> /dev/null
+done
---
+++ zenity_rsync.bash
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# 2011 (CC0) Bartosz Szczesny <bszcz@bszcz.eu>
+
+NOBK="/home/nobackup/"$USER
+ls -ld $NOBK
+
+zenity --info --title "bsync: Hello!" --text "1. Choose source directory\n2. Choose target directory\n3. Directories are 'rsynced'"
+
+src=$(zenity --file-selection --directory --save --filename=$HOME/ --title "bsync: Choose source directory")"/" # adding "/" at the end
+if [ "$src" == "/" ]
+then
+  zenity --warning --title "bsync: Warning" --text "bsync: Source directory not chosen, please start again."
+  exit
+fi
+
+trg=$(zenity --file-selection --directory --save --filename=$NOBK/ --title "bsync: Choose target directory")"/" # adding "/" at the end
+if [ "$trg" == "/" ]
+then
+  zenity --warning --title "bsync: Warning" --text "Target directory not chosen, please start again."
+  exit
+fi
+
+rsync -varz $src $trg | zenity --progress --pulsate --title "bsync: 'Rsyncing'..." --text "You can press OK when 'rsyncing' is finished."
+
+
