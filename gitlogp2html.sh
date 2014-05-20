929d44192014ecda76c909a248ad1b3518efa5bc
Bartosz Szczesny <bszcz@bszcz.org>
2014-05-20 23:57:48 +0100

    simple diff2html tool for 'git log -p' output

---
+++ gitlogp2html.sh
@@ -0,0 +1,13 @@
+# Copyright (c) 2014 Bartosz Szczesny <bszcz@bszcz.org>
+# This program is free software under the MIT license.
+
+echo "<pre style=\"white-space:pre-wrap\">" > diff.html
+git log -p --no-color --word-diff=porcelain \
+| sed 's/>/\&gt;/g' \
+| sed 's/</\&lt;/g' \
+| sed 's/^commit\(.*\)$/<span style="color:orange">commit\1<\/span>/' \
+| sed 's/^@@ \([^@]*\) @@/<span style="color:blue">@@ \1 @@<\/span>/' \
+| sed 's/^-\([^-].*\)$/<span style="color:red">-\1<\/span>/' \
+| sed 's/^+\([^+].*\)$/<span style="color:green">+\1<\/span>/' \
+>> diff.html
+echo "</pre>" >> diff.html
