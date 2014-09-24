20b38abe0001c16059f6c848f4acb904938342ee
Bartosz Szczesny <bszcz@bszcz.org>
2014-09-24 20:23:03 +0100

    fix indentation

--- color64.bash
+++ color64.bash
@@ -7,12 +7,12 @@ tmp_pgm=onepix.pgm
 tmp_ppm=onepix.ppm
 
 for image in $@; do
-    convert -resize 1x1 $image $tmp_pgm &
-    convert -resize 1x1 $image $tmp_ppm &
-    wait
-    pixel_pgm=$(tail -n1 $tmp_pgm | hexdump -v -e '/1 "%02X"')
-    pixel_ppm=$(tail -n1 $tmp_ppm | hexdump -v -e '/1 "%02X"')
-    echo $pixel_pgm $pixel_ppm $image
+	convert -resize 1x1 $image $tmp_pgm &
+	convert -resize 1x1 $image $tmp_ppm &
+	wait
+	pixel_pgm=$(tail -n1 $tmp_pgm | hexdump -v -e '/1 "%02X"')
+	pixel_ppm=$(tail -n1 $tmp_ppm | hexdump -v -e '/1 "%02X"')
+	echo $pixel_pgm $pixel_ppm $image
 done
 
 rm $tmp_pgm
