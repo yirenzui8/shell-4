ae617eed7381c4513059d8fd90c01ef120e8779a
Bartosz Szczesny <bszcz@bszcz.org>
2014-09-25 22:41:53 +0100

    add 1x2 images, handle newlines, force exact resizing

--- hexcolor.bash
+++ hexcolor.bash
@@ -4,16 +4,25 @@
 # This program is free software under the MIT license.
 
 tmp1x1pgm=tmp1x1.pgm
+tmp1x2pgm=tmp1x2.pgm
 tmp1x1ppm=tmp1x1.ppm
+tmp1x2ppm=tmp1x2.ppm
 
 for image in $@; do
-	convert -resize 1x1 $image $tmp1x1pgm &
-	convert -resize 1x1 $image $tmp1x1ppm &
+	# \! = force resize, ignore aspect ratio
+	convert -resize 1x1\! $image $tmp1x1pgm &
+	convert -resize 1x2\! $image $tmp1x2pgm &
+	convert -resize 1x1\! $image $tmp1x1ppm &
+	convert -resize 1x2\! $image $tmp1x2ppm &
 	wait
-	hex1x1pgm=$(tail -n1 $tmp1x1pgm | hexdump -v -e '/1 "%02X"')
-	hex1x1ppm=$(tail -n1 $tmp1x1ppm | hexdump -v -e '/1 "%02X"')
-	echo $hex1x1pgm $hex1x1ppm $image
+	hex1x1pgm=$(tail -n+4 $tmp1x1pgm | hexdump -v -e '/1 "%02X"')
+	hex1x2pgm=$(tail -n+4 $tmp1x2pgm | hexdump -v -e '/1 "%02X"')
+	hex1x1ppm=$(tail -n+4 $tmp1x1ppm | hexdump -v -e '/1 "%02X"')
+	hex1x2ppm=$(tail -n+4 $tmp1x2ppm | hexdump -v -e '/1 "%02X"')
+	echo $hex1x1pgm $hex1x2pgm $hex1x1ppm $hex1x2ppm $image
+	rm $tmp1x1pgm
+	rm $tmp1x2pgm
+	rm $tmp1x1ppm
+	rm $tmp1x2ppm
 done
 
-rm $tmp1x1pgm
-rm $tmp1x1ppm
